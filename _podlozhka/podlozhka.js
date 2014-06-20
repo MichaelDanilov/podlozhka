28d0a1e85c8e490a0b3cf8a3356909f2b8d2681d
Michael Danilov <michael@danilov.me>
2014-06-20 18:50:04 +0700

    [*] Change double quote to single

--- _podlozhka/podlozhka.js
+++ _podlozhka/podlozhka.js
@@ -1,31 +1,31 @@
 (function($,undefined) {
-  "use strict";
+  'use strict';
 
-  var $body_elems = $("body").children(), // root
+  var $body_elems = $('body').children(), // root
   $podlozhka = {},
   $form = {},
   $event = {};
 
   // inserted html
-  $podlozhka.html = "<div class=\"dnlv-podlozhka__body\"></div><div class=\"dnlv-podlozhka__img\"></div><div class=\"dnlv-podlozhka__form dnlv-podlozhka-twbs\"><div class=\"modal-content\"><div class=\"modal-header\"><h4 class=\"modal-title\">Podlozhka</h4></div><div class=\"modal-body\"><div class=\"form-group\"><label for=\"dnlv-podlozhka-form-imgsrc\">Path of image</label><input type=\"text\" class=\"form-control\" id=\"dnlv-podlozhka-form-imgsrc\" placeholder=\"_podlozhka/img/img.png\" value=\"_podlozhka/img/img.png\"></div><div class=\"form-group\"><label>Margins</label><div class=\"form-inline\"><div class=\"input-group\"><span class=\"input-group-addon\">left:</span><input type=\"text\" class=\"form-control\" id=\"dnlv-podlozhka-form-imgx\"><span class=\"input-group-addon\">px</span></div><div class=\"input-group\"><span class=\"input-group-addon\">top:</span><input type=\"text\" class=\"form-control\" id=\"dnlv-podlozhka-form-imgy\"><span class=\"input-group-addon\">px</span></div></div></div></div></div></div>";
+  $podlozhka.html = '<div class="dnlv-podlozhka__body"></div><div class="dnlv-podlozhka__img"></div><div class="dnlv-podlozhka__form dnlv-podlozhka-twbs"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">Podlozhka</h4></div><div class="modal-body"><div class="form-group"><label for="dnlv-podlozhka-form-imgsrc">Path of image</label><input type="text" class="form-control" id="dnlv-podlozhka-form-imgsrc" placeholder="_podlozhka/img/img.png" value="_podlozhka/img/img.png"></div><div class="form-group"><label>Margins</label><div class="form-inline"><div class="input-group"><span class="input-group-addon">left:</span><input type="text" class="form-control" id="dnlv-podlozhka-form-imgx"><span class="input-group-addon">px</span></div><div class="input-group"><span class="input-group-addon">top:</span><input type="text" class="form-control" id="dnlv-podlozhka-form-imgy"><span class="input-group-addon">px</span></div></div></div></div></div></div>';
   // styles
-  $podlozhka.css = "<link rel=\"stylesheet\" href=\"_podlozhka/podlozhka.min.css\">";
-  $podlozhka.img_src = "";
+  $podlozhka.css = '<link rel="stylesheet" href="_podlozhka/podlozhka.min.css">';
+  $podlozhka.img_src = '';
 
-  $("body").append($podlozhka.html);
-  $("head").append($podlozhka.css);
+  $('body').append($podlozhka.html);
+  $('head').append($podlozhka.css);
 
-  $podlozhka.body = $(".dnlv-podlozhka__body");
-  $podlozhka.img_wrap = $(".dnlv-podlozhka__img");
+  $podlozhka.body = $('.dnlv-podlozhka__body');
+  $podlozhka.img_wrap = $('.dnlv-podlozhka__img');
   
-  $form.img_src = $("#dnlv-podlozhka-form-imgsrc");
-  $form.x = $("#dnlv-podlozhka-form-imgx");
-  $form.y = $("#dnlv-podlozhka-form-imgy");
+  $form.img_src = $('#dnlv-podlozhka-form-imgsrc');
+  $form.x = $('#dnlv-podlozhka-form-imgx');
+  $form.y = $('#dnlv-podlozhka-form-imgy');
 
   $body_elems.each(function(){
     var $this = $(this),
     elem;
-    if ($this.nodeName !== "script") {
+    if ($this.nodeName !== 'script') {
       elem = $this.detach();
       $podlozhka.body.append(elem);
     }
@@ -45,19 +45,19 @@
     $form.y.val(parseFloat(y));
   }
 
-  $form.img_src.on("keyup",function () {
+  $form.img_src.on('keyup',function () {
 
     $podlozhka.img_src = $form.img_src.val();
 
     $podlozhka.img_wrap.css({
-      "background-image": "url(" + $podlozhka.img_src + ")"
+      'background-image': 'url(' + $podlozhka.img_src + ')'
     });
 
     $event.mousedown = false;
 
     changeImgPositions($podlozhka.mouse_positions.x, $podlozhka.mouse_positions.y);
 
-    $podlozhka.img_wrap.on("mousedown",function(event){
+    $podlozhka.img_wrap.on('mousedown',function(event){
       if (!$event.mousedown) {
         $event.mousedown = true;
         $podlozhka.mouse_positions.x = event.pageX;
@@ -66,22 +66,22 @@
       }
     });
 
-    $podlozhka.img_wrap.on("mousemove",function(event){
+    $podlozhka.img_wrap.on('mousemove',function(event){
       var x,y;
       if ($event.mousedown) {
         x = $podlozhka.img_positions.x + (event.pageX - $podlozhka.mouse_positions.x);
         y = $podlozhka.img_positions.y + (event.pageY - $podlozhka.mouse_positions.y);
         $podlozhka.img_wrap.css({
-          "background-position": x + "px " + y + "px"
+          'background-position': x + 'px ' + y + 'px'
         });
       }
     });
 
-    $podlozhka.img_wrap.on("mouseup",function(){
+    $podlozhka.img_wrap.on('mouseup',function(){
       var $positions;
       if ($event.mousedown) {
         $event.mousedown = false;
-        $positions = $podlozhka.img_wrap.css("background-position").match(/^([0-9\-\+\.]*)px ([0-9\-\+\.]*)px$/i);
+        $positions = $podlozhka.img_wrap.css('background-position').match(/^([0-9\-\+\.]*)px ([0-9\-\+\.]*)px$/i);
         if ($positions) {
           $podlozhka.img_positions.x = parseFloat($positions[1]);
           $podlozhka.img_positions.y = parseFloat($positions[2]);
@@ -92,17 +92,17 @@
     });
   });
 
-  $form.x.on("keyup",function () {
+  $form.x.on('keyup',function () {
     $podlozhka.img_positions.x = parseFloat($(this).val());
     $podlozhka.img_wrap.css({
-      "background-position": $podlozhka.img_positions.x + "px " + $podlozhka.img_positions.y + "px"
+      'background-position': $podlozhka.img_positions.x + 'px ' + $podlozhka.img_positions.y + 'px'
     });
   });
 
-  $form.y.on("keyup",function () {
+  $form.y.on('keyup',function () {
     $podlozhka.img_positions.y = parseFloat($(this).val());
     $podlozhka.img_wrap.css({
-      "background-position": $podlozhka.img_positions.x + "px " + $podlozhka.img_positions.y + "px"
+      'background-position': $podlozhka.img_positions.x + 'px ' + $podlozhka.img_positions.y + 'px'
     });
   });
 
